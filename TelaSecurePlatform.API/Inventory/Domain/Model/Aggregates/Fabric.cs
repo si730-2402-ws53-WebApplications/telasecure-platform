d642a6206d780a2f1ca4bcabe6b01c0ba1b0e9c5
John_Arvlo <u202117377@upc.edu.pe>
2024-11-01 17:27:19 -0500

    feat: added fabric aggregate logic to inventory bounded context.

---
+++ TelaSecurePlatform.API/Inventory/Domain/Model/Aggregates/Fabric.cs
@@ -0,0 +1,20 @@
+﻿namespace TelaSecurePlatform.API.Inventory.Domain.Model.Aggregates;
+
+public class Fabric
+{
+    public int Id { get; }
+    //public string Code { get; private set; }
+    public string Name { get; private set; }
+    public int StoreroomId { get; private set; }
+    //public Category CategoryId { get; private set; }
+    public int CategoryId { get; private set; }
+    public int Quantity { get; private set; }
+    
+    public Fabric(string name, int storeroomId, int categoryId, int quantity)
+    {
+        Name = name;
+        StoreroomId = storeroomId;
+        CategoryId = categoryId;
+        Quantity = quantity;
+    }
+}
--- TelaSecurePlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ TelaSecurePlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,5 +1,6 @@
 using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
 using Microsoft.EntityFrameworkCore;
+using TelaSecurePlatform.API.Inventory.Domain.Model.Aggregates;
 using TelaSecurePlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
 
 namespace TelaSecurePlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
@@ -15,15 +16,14 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-        /*
-        builder.Entity<FavoriteSource>().HasKey(f => f.Id);
-        builder.Entity<FavoriteSource>().Property(f => f.Id)
-            .IsRequired().ValueGeneratedOnAdd();
-        builder.Entity<FavoriteSource>().Property(f => f.NewsApiKey)
-            .IsRequired();
-        builder.Entity<FavoriteSource>().Property(f => f.SourceId)
-            .IsRequired();
-        */
+        
+        builder.Entity<Fabric>().HasKey(f => f.Id);
+        builder.Entity<Fabric>().Property(f => f.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<Fabric>().Property(f => f.Name).IsRequired().HasMaxLength(30);
+        builder.Entity<Fabric>().Property(f => f.StoreroomId).IsRequired();
+        builder.Entity<Fabric>().Property(f => f.CategoryId).IsRequired();
+        builder.Entity<Fabric>().Property(f => f.Quantity).IsRequired();
+        
         builder.UseSnakeCaseNamingConvention();
     }
 }
