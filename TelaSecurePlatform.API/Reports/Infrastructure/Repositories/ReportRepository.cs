a028507fe8dc189033bd3dffd964b4ad3e188fca
omele7 <dicabrerabuitron@gmail.com>
2024-11-02 12:52:03 -0500

    feat(report): added the repository.

---
+++ TelaSecurePlatform.API/Reports/Infrastructure/Repositories/ReportRepository.cs
@@ -0,0 +1,21 @@
+﻿/*
+
+using Microsoft.EntityFrameworkCore;
+using TelaSecurePlatform.API.Reports.Domain.Model.Aggregates;
+using TelaSecurePlatform.API.Reports.Domain.Repositories;
+using TelaSecurePlatform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace TelaSecurePlatform.API.Reports.Infrastructure.Persistence.Repositories;
+
+public class ReportRepository(AppDbContext context) : BaseRepository<Report>(context), IReportRepository
+{
+    public new async Task<Report?> FindByIdAsync(int id) =>
+        await Context.Set<Report>()
+            .Where(r => r.Id == id)
+            .FirstOrDefaultAsync();
+
+    public new async Task<IEnumerable<Report>> ListAsync() =>
+        await Context.Set<Report>().ToListAsync();
+}
+
+*/
