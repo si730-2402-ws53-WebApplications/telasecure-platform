c0b488695eb65556974b43b09b92e99bb6141ac3
omele7 <dicabrerabuitron@gmail.com>
2024-11-02 12:51:21 -0500

    feat(report): added the command services.

---
+++ TelaSecurePlatform.API/Reports/Application/Internal/CommandServices/ReportCommandService.cs
@@ -0,0 +1,25 @@
+﻿/*
+
+using TelaSecurePlatform.API.Reports.Domain.Model.Aggregates;
+using TelaSecurePlatform.API.Reports.Domain.Model.Commands;
+using TelaSecurePlatform.API.Reports.Domain.Repositories;
+using TelaSecurePlatform.API.Reports.Domain.Services;
+using TelaSecurePlatform.API.Shared.Domain.Repositories;
+
+namespace TelaSecurePlatform.API.Reports.Application.Internal.CommandServices;
+
+public class ReportCommandService(
+    IReportRepository reportRepository,
+    IUnitOfWork unitOfWork)
+    : IReportCommandService
+{
+    public async Task<Report?> Handle(CreateReportCommand command)
+    {
+        var report = new Report(command.Title, command.Content, command.AuthorId);
+        await reportRepository.AddAsync(report);
+        await unitOfWork.CompleteAsync();
+        return report;
+    }
+}
+
+*/
